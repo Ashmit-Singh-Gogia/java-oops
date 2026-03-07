21f2ad243428e1b30f4e739149893bffa6fe5a37
Ashmit Singh Gogia <ashmit9955@gmail.com>
2026-03-08 01:07:43 +0530

    1 to 1 Association

---
+++ Relationship/Association/Main.java
@@ -0,0 +1,32 @@
+package Relationship.Association;
+
+class Passport{
+    private String passportNumber;
+    Passport(String passportNumber){
+        this.passportNumber = passportNumber;
+    }
+    public String getPassportNumber(){
+        return passportNumber;
+    }
+}
+
+class Student{
+    private String name;
+    private Passport passport;
+    Student(String name , Passport passport){
+        this.name = name;
+        this.passport = passport;
+    }
+    public void displayDetails(){
+        System.out.println("Student name: " + name);
+        System.out.println("Passport : " + passport.getPassportNumber());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Passport passport = new Passport("9949293949292");
+        Student student = new Student("Riya", passport);
+        student.displayDetails();
+    }
+}
